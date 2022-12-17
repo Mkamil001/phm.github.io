3e6f29fb0456db541184be1b147dc718b81793e7
Mkamil001 <113684231+Mkamil001@users.noreply.github.com>
2022-12-17 05:01:52 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open')
+  nav.classList.toggle('flex')
+  nav.classList.toggle('hidden')
+})
+
+var swiper = new Swiper(".books-slider", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 1,
+      },
+    },
+  });
